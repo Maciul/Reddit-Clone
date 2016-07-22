61f3003c5ad23a73ed5ce5b6ee3379634c0ee695
Pawel <Pawel.Maciulewski@gmail.com>
2016-07-21 22:01:27 -0600

    include add new post feature

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ app.factory('factory', [
           image: 'http://img06.deviantart.net/bf45/i/2010/157/d/5/olsztyn_old_city_hdr_by_unsu.jpg',
           author: 'Pawel Maciulewski',
           description: 'Lorem ipsum dolor sit amet, vero vocent principes id nec, et nemore accumsan rationibus eos, ne sed virtute utroque. Atqui quodsi laboramus eos ei, sit at sanctus nusquam. No vocent hendrerit mea, graeci interpretaris at cum. Et per elit graeci eloquentiam, ullum errem principes his ex. Corrumpit abhorreant in vix, an vero errem quo. Duo cu platonem gloriatur, vis primis viderer ad.',
-          date: new Date()
+          date: new Date(),
+          vote: 0
         },
         {
           id: ++counter,
@@ -18,7 +19,8 @@ app.factory('factory', [
           image: 'http://img02.deviantart.net/94c8/i/2014/335/8/4/shadows___free_stock_by_heavensinyoureyes-d88detg.jpg',
           author: 'Sara Rodriguez',
           description: 'Lorem ipsum dolor sit amet, vero vocent principes id nec, et nemore accumsan rationibus eos, ne sed virtute utroque. Atqui quodsi laboramus eos ei, sit at sanctus nusquam. No vocent hendrerit mea, graeci interpretaris at cum. Et per elit graeci eloquentiam, ullum errem principes his ex. Corrumpit abhorreant in vix, an vero errem quo. Duo cu platonem gloriatur, vis primis viderer ad.',
-          date: new Date()
+          date: new Date(),
+          vote: 0
         },
         {
           id: ++counter,
@@ -26,7 +28,8 @@ app.factory('factory', [
           image: 'http://img10.deviantart.net/e578/i/2008/275/4/2/maastricht_v_by_ayseselen.jpg',
           author: 'Rick Van Der Van',
           description: 'Lorem ipsum dolor sit amet, vero vocent principes id nec, et nemore accumsan rationibus eos, ne sed virtute utroque. Atqui quodsi laboramus eos ei, sit at sanctus nusquam. No vocent hendrerit mea, graeci interpretaris at cum. Et per elit graeci eloquentiam, ullum errem principes his ex. Corrumpit abhorreant in vix, an vero errem quo. Duo cu platonem gloriatur, vis primis viderer ad.',
-          date: new Date()
+          date: new Date(),
+          vote: 0
         }
       ];
 
@@ -53,12 +56,24 @@ app.factory('factory', [
     }
   }
 
+  function addNewPost(form) {
+    console.log(form);
+      posts.push({
+        id: ++counter,
+        title: form.title,
+        image: form.image,
+        author: form.author,
+        description: form.description,
+        date: new Date()
+       });
+    }
+
       return {
         posts: function() { return posts },
         choices: function() {return choices},
         plusVote: plusVote,
-        minusVote: minusVote
-
+        minusVote: minusVote,
+        addNewPost: addNewPost
       };
     }
 ]);
@@ -71,5 +86,6 @@ app.controller("FirstControl", [
      main.choices = f.choices();
      main.plusVote = f.plusVote;
      main.minusVote = f.minusVote;
+     main.addNewPost = f.addNewPost;
    }
  ]);
